aaf3e94455e054b70d7a1107beec14038ba5be6e
SoinDosa <rlatjsgnl519@gmail.com>
2021-07-01 18:05:05 +0900

    add

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour
 {
+    float bulletSpeed = 10.0f;
+    Vector3 firstPos;
+    Vector3 curPos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        firstPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(0,0, bulletSpeed * Time.deltaTime);
+        curPos = transform.position;
+        if(Vector3.Distance(firstPos, curPos) > 10f)
+        {
+            Destroy(this.gameObject);
+        }
     }
 
     public void OnTriggerStay(Collider other)
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     float playerSpeed = 10f;
+    bool hitDelayCheck = true;
+
+    public GameObject bullet;
+    bool shootDelayCheck = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,5 +19,32 @@ public class PlayerScript : MonoBehaviour
     void Update()
     {
         transform.Translate(Input.GetAxis("Horizontal") * playerSpeed * Time.deltaTime, 0, Input.GetAxis("Vertical") * playerSpeed * Time.deltaTime);
+
+        if(Input.GetMouseButton(0) && shootDelayCheck)
+        {
+            StartCoroutine("Shoot");
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if(collision.gameObject.tag == "Enemy" && hitDelayCheck){
+            StartCoroutine("HitCheck");
+        }
+    }
+    IEnumerator HitCheck()
+    {
+        hitDelayCheck = false;
+        GameManager.PlayerHit();
+        yield return new WaitForSeconds(1);
+        hitDelayCheck = true;
+    }
+
+    IEnumerator Shoot()
+    {
+        shootDelayCheck = false;
+        Instantiate(bullet, this.transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(1);
+        shootDelayCheck = true;
     }
 }
